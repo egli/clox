abc9d85c41367e1ad8a5b72ace406ded22fc571e
Christian Egli <christian@egli.cc>
2025-08-26 12:08:00 +0200

    Start from scratch

--- src/chunk.h
+++ src/chunk.h
@@ -6,18 +6,6 @@
 
 typedef enum {
   OP_CONSTANT,
-  OP_NIL,
-  OP_TRUE,
-  OP_FALSE,
-  OP_EQUAL,
-  OP_GREATER,
-  OP_LESS,
-  OP_ADD,
-  OP_SUBTRACT,
-  OP_MULTIPLY,
-  OP_DIVIDE,
-  OP_NOT,
-  OP_NEGATE,
   OP_RETURN,
 } OpCode;
 
@@ -25,8 +13,8 @@ typedef struct {
   int count;
   int capacity;
   uint8_t* code;
-  int* lines;
   ValueArray constants;
+  int* lines;
 } Chunk;
 
 void initChunk(Chunk* chunk);
--- src/value.c
+++ src/value.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -16,6 +15,10 @@ void freeValueArray(ValueArray* array) {
   initValueArray(array);
 }
 
+void printValue(Value value) {
+  printf("%g", value);
+}
+
 void writeValueArray(ValueArray* array, Value value) {
   if (array->capacity < array->count + 1) {
     int oldCapacity = array->capacity;
@@ -27,29 +30,3 @@ void writeValueArray(ValueArray* array, Value value) {
   array->count++;
 }
 
-void printValue(Value value) {
-  switch (value.type) {
-  case VAL_BOOL:
-    printf(AS_BOOL(value) ? "true" : "false");
-    break;
-  case VAL_NIL: printf("nil"); break;
-  case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
-  case VAL_OBJ: printObj(value); break;
-  }
-}
-
-bool valuesEqual(Value a, Value b) {
-  if (a.type != b.type) return false;
-  switch (a.type) {
-  case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
-  case VAL_NIL:    return true;
-  case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
-  case VAL_OBJ: {
-    ObjString* aString = AS_STRING(a);
-    ObjString* bString = AS_STRING(b);
-    return aString->length == bString->length &&
-      memcmp(aString->chars, bString->chars, aString->length) == 0;
-  }
-  default:         return false;
-  }
-}
