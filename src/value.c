8b49db35b84302df42ed83f9b2cb033fc04e5495
Christian Egli <christian@egli.cc>
2025-11-12 20:28:45 +0100

    Implement comparisons

--- src/value.c
+++ src/value.c
@@ -24,6 +24,16 @@ void printValue(Value value) {
   }
 }
 
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type) return false;
+  switch (a.type) {
+  case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+  case VAL_NIL:    return true;
+  case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+  default:         return false; // Unreachable.
+  }
+}
+
 void writeValueArray(ValueArray* array, Value value) {
   if (array->capacity < array->count + 1) {
     int oldCapacity = array->capacity;
