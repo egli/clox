06c03f3f862a5a97fe9589fb67c008259a2a59ac
Christian Egli <christian@egli.cc>
2026-01-08 08:50:34 +0100

    Fix a bug in the disassembling of globals
    They are constant instructions not simple ones

--- src/debug.c
+++ src/debug.c
@@ -62,9 +62,9 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   case OP_POP:
     return simpleInstruction("OP_POP", offset);
   case OP_GET_GLOBAL:
-    return simpleInstruction("OP_GET_GLOBAL", offset);
+    return constantInstruction("OP_GET_GLOBAL", chunk, offset);
   case OP_DEFINE_GLOBAL:
-    return simpleInstruction("OP_DEFINE_GLOBAL", offset);
+    return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
   case OP_SET_GLOBAL:
     return constantInstruction("OP_SET_GLOBAL", chunk, offset);
   case OP_EQUAL:
