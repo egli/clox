4c7385dd2316f68e1fc242d14395ce6b6ac20ce9
Christian Egli <christian@egli.cc>
2026-03-06 10:45:51 +0100

    Implement while

--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -190,6 +191,11 @@ static InterpretResult run() {
     case OP_JUMP_IF_FALSE: {
       uint16_t offset = READ_SHORT();
       if (isFalsey(peek(0))) vm.ip += offset;
+      break;
+    }
+    case OP_LOOP: {
+      uint16_t offset = READ_SHORT();
+      vm.ip -= offset;
       break;
     }
     case OP_RETURN:
