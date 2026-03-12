8a67feefe31eb3b733cd6eadfdf8f5b7a217a15b
Christian Egli <christian@egli.cc>
2026-03-12 09:06:03 +0100

    Implement calls and functions

--- src/debug.c
+++ src/debug.c
@@ -41,8 +41,7 @@ static int jumpInstruction(const char* name, int sign, Chunk* chunk, int offset)
 
 int disassembleInstruction(Chunk* chunk, int offset) {
   printf("%04d ", offset);
-  if (offset > 0 &&
-      chunk->lines[offset] == chunk->lines[offset - 1]) {
+  if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
     printf("   | ");
   } else {
     printf("%4d ", chunk->lines[offset]);
@@ -72,6 +71,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
   case OP_LOOP:
     return jumpInstruction("OP_LOOP", -1, chunk, offset);
+  case OP_CALL:
+    return byteInstruction("OP_CALL", chunk, offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   case OP_NIL:
